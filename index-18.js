b53c1265a85c0b2619e33be27a83c9fc9079f1cf
Matvej-Povroznyk <matvejtop12@gmail.com>
2026-05-10 10:50:01 +0300

    UPDATED TEMPLATE

---
+++ index-18.js
@@ -0,0 +1,40 @@
+/* START OF HW-18 */
+/* TASKS 1-4: NOT READY */
+
+/* TASK 1 */
+
+
+/* TASK 2 */
+const ingredients = [
+ 'Картопля',
+ 'Гриби',
+ 'Часник',
+ 'Помідори',
+ 'Зелень',
+ 'Приправи',
+];
+
+/* TASK 3 */
+const images = [
+ {
+  url:
+   'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  alt: 'White and Black Long Fur Cat',
+ },
+ {
+  url:
+   'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+ },
+ {
+  url:
+   'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  alt: 'Group of Horses Running',
+ },
+];
+
+/* TASK 4 */
+let counterValue = 0;
+
+function increment() {}
+function decrement() {}
